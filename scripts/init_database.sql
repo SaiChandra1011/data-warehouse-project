51b60e0a5135bec8496413565a36455033b642a4
Bommena Sai Chandra Vardhan Rao <saichandra1001@gmail.com>
2025-08-02 22:23:47 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,28 @@
+/*
+
+Scrip summary - created this script to create the databse named 'warehouse' and it also check if a databse named 'warehouse'already exists, also this
+script creates the medallion architecture - bronze, silver, gold schemas
+*/
+
+-- Caution  - if this script is run , it deleted the existing warehouse databse, so proceed with caution.
+
+USE master;
+CREATE DATABASE warehouse;
+ 
+ USE warehouse;
+
+-- drop existing database if already created
+IF EXISTS (SELECT 1 FROM sys.database WHERE name = 'warehouse')
+BEGIN
+        ALTER DATABASE warehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+        DROp DATABASE warehouse;
+END;
+GO
+
+-- creating the bronze, silver, gold schemas 
+ CREATE SCHEMA bronze;
+ GO
+ CREATE SCHEMA silver;
+ GO 
+ CREATE SCHEMA gold;
+ GO
