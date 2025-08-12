846705a1cdeb6f3c22350c48a55ca3f23c0b531d
Bommena Sai Chandra Vardhan Rao <saichandra1001@gmail.com>
2025-08-12 22:13:46 +0530

    updates to new schema to load cleaned data to silver

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -30,12 +30,13 @@ IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 GO
 CREATE TABLE silver.crm_prd_info(
     prd_id INT,
+    cat_id NVARCHAR(50),
     prd_key NVARCHAR(50),
     prd_nm NVARCHAR(50),
     prd_cost INT,
     prd_line NVARCHAR(50),
-    prd_start_date DATETIME,
-    prd_end_date DATETIME,
+    prd_start_date DATE,
+    prd_end_date DATE,
     dwh_create_date DATETIME2 DEFAULT GETDATE()
 
 );
